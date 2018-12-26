e9fdfbefc40e3ab2c9b61594dc29900b8e1f9ad0
Nate Huff <nhuff90@gmail.com>
2018-12-26 10:42:20 -0500

    Add exception handling to deploy python script

--- upload-portfolio-lamdba.py
+++ upload-portfolio-lamdba.py
@@ -5,20 +5,28 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:117837584658:deployPortfolioTopic')
 
-    portfolio_bucket = s3.Bucket('portfolio.natehuff.info')
+    try:
+        s3 = boto3.resource('s3')
 
-    build_bucket = s3.Bucket('portfoliobuild.natehuff.info')
+        portfolio_bucket = s3.Bucket('portfolio.natehuff.info')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket = s3.Bucket('portfoliobuild.natehuff.info')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    return 'Hello from Lambda'
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        return 'Deploy complete.'
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed.", Message="The portfolio failed to deploy.")
+        raise 
